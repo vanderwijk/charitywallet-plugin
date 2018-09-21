8d4206e55bc0ec97d4b170ff9473c53740fde447
Johan van der Wijk <johan@vanderwijk.nl>
2018-09-21 14:05:39 +0200

    First commit.

---
+++ charitywallet-plugin.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Plugin Name:       Charity Wallet
+ * Plugin URI:        https://charitywallet.com
+ * Description:       Charity Wallet WordPress plugin
+ * Version:           1.0.0
+ * Author:            Johan van der Wijk
+ * Author URI:        https://thewebworks.nl
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       charitywallet-plugin
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
