0d9c2fed58d42e70a109c73c6efb1ef0ae1059b6
Johan van der Wijk <johan@vanderwijk.nl>
2018-10-23 15:43:02 +0200

    Transaction description.

--- shortcode-wallet.php
+++ shortcode-wallet.php
@@ -109,7 +109,7 @@ function chawa_display_wallet() {
 						"value" => $amount // You must send the correct number of decimals, thus we enforce the use of strings
 					],
 					"method" => \Mollie\Api\Types\PaymentMethod::IDEAL,
-					"description" => 'Wallet' . __('Transaction','chawa') . '#{$orderId}',
+					"description" => 'Wallet ' . __('Transaction','chawa') . ' #{$orderId}',
 					"redirectUrl" => "{$protocol}://{$hostname}{$path}/payments/return.php?order_id={$orderId}",
 					"webhookUrl" => "{$protocol}://{$hostname}{$path}/payments/webhook.php",
 					"metadata" => [
