67d2056518c6b15d02555e9170df37e72b65e1a8
Johan van der Wijk <johan@vanderwijk.nl>
2019-10-23 13:43:25 +0200

    Print

--- shortcodes/onboarding/onboarding-pay.php
+++ shortcodes/onboarding/onboarding-pay.php
@@ -159,7 +159,7 @@
 		header("Location: " . $payment->getCheckoutUrl(), true, 303);
 	} ?>
 
-	<?php return ob_get_clean(); // use return if shortcode
+	<?php print ob_get_clean(); // use return if shortcode
 	} catch (\Mollie\Api\Exceptions\ApiException $e) {
 		echo "API call failed: " . htmlspecialchars($e->getMessage());
 	} ?>
