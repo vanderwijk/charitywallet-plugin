e75e03b3e41c297b6b0dcaaa0acd8856c917214d
Johan van der Wijk <johan@vanderwijk.nl>
2020-01-22 15:28:34 +0100

    Database

--- functions/database-transactions.php
+++ functions/database-transactions.php
@@ -13,7 +13,7 @@ function chawa_db_transactions_install() {
 	$charset_collate = $wpdb->get_charset_collate();
 
 	$sql = "CREATE TABLE $table_name (
-		transaction_id varchar(50) NULL,
+		transaction_id varchar(50) DEFAULT 'chawa_000000000000000000000000' NULL,
 		transaction_type varchar(50) NOT NULL,
 		transaction_status varchar(50) NOT NULL,
 		source_id varchar(50) NOT NULL,
@@ -66,7 +66,7 @@ if ($installed_ver != $chawa_table_ver_transactions) {
 	$table_name = $wpdb->prefix . 'chawa_transactions';
 
 	$sql = "CREATE TABLE $table_name (
-		transaction_id varchar(50) NULL,
+		transaction_id varchar(50) DEFAULT 'chawa_000000000000000000000000' NULL,
 		transaction_type varchar(50) NOT NULL,
 		transaction_status varchar(50) NOT NULL,
 		source_id varchar(50) NOT NULL,
