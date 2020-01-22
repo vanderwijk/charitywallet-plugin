2522aca79f47caeef60e5ef93960bd89fb0c8e5a
Johan van der Wijk <johan@vanderwijk.nl>
2020-01-22 15:17:49 +0100

    Check

--- functions/database-donations.php
+++ functions/database-donations.php
@@ -16,14 +16,14 @@ function chawa_db_donations_install() {
 	$sql = "CREATE TABLE $table_name_donations (
 		donation_id mediumint(9) NOT NULL AUTO_INCREMENT,
 		donation_status varchar(50) NOT NULL,
-		transaction_id varchar(50) NOT NULL,
+		transaction_id varchar(50) NULL,
 		charity_id varchar(50) NOT NULL,
 		amount varchar(50) NOT NULL,
-		time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+		time datetime DEFAULT '1970-01-01 00:00:01' NOT NULL,
 		PRIMARY KEY (donation_id),
 		FOREIGN KEY (transaction_id) 
 			REFERENCES wp_e8ab437ebb_chawa_transactions(transaction_id) 
-			ON DELETE NULL
+			ON DELETE SET NULL
 	) $charset_collate;";
 
 	require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
@@ -61,14 +61,15 @@ if ($installed_ver != $chawa_table_ver_donations) {
 	$sql = "CREATE TABLE $table_name_donations (
 		donation_id mediumint(9) NOT NULL AUTO_INCREMENT,
 		donation_status varchar(50) NOT NULL,
-		transaction_id varchar(50) NOT NULL,
+		transaction_id varchar(50) NULL,
 		charity_id varchar(50) NOT NULL,
 		amount varchar(50) NOT NULL,
-		time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+		time datetime DEFAULT '1970-01-01 00:00:01' NOT NULL,
 		PRIMARY KEY (donation_id),
-		FOREIGN KEY (transaction_id) 
-			REFERENCES wp_e8ab437ebb_chawa_transactions(transaction_id) 
-			ON DELETE NULL
+		CONSTRAINT fk_transaction_id
+			FOREIGN KEY (transaction_id) 
+			REFERENCES wp_e8ab437ebb_chawa_transactions (transaction_id) 
+			ON DELETE SET NULL
 	);";
 
 	require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
