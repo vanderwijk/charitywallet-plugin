9ba40a6f95181e6e01aa41504cc854e4741c2d50
Johan van der Wijk <johan@vanderwijk.nl>
2020-01-22 16:00:45 +0100

    References

--- charitywallet-plugin.php
+++ charitywallet-plugin.php
@@ -21,7 +21,7 @@ if ( ! defined('WPINC' )) {
 define('CHAWA_PLUGIN_DIR', plugin_dir_url(__FILE__));
 define('CHAWA_PLUGIN_DIR_PATH', plugin_dir_path(__FILE__)); // Relative path
 define('CHAWA_PLUGIN_VER', '1.0.1');
-define('CHAWA_DATABASE_VER', '1.3.6');
+define('CHAWA_DATABASE_VER', '1.3.7');
 
 // database
 define('CHAWA_TABLE_TRANSACTIONS', $wpdb->prefix . 'chawa_transactions');
--- functions/database-donations.php
+++ functions/database-donations.php
@@ -23,7 +23,7 @@ function chawa_db_donations_install() {
 		PRIMARY KEY (donation_id),
 		CONSTRAINT fk_transaction_id
 			FOREIGN KEY (transaction_id) 
-			REFERENCES wp_e8ab437ebb_chawa_transactions (transaction_id) 
+			REFERENCES $table_name_transactions (transaction_id) 
 			ON DELETE SET NULL
 	) $charset_collate;";
 
